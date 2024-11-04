9efb9cce9886e837fe823660240bd84f658fdd45
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-11-04 15:51:01 +0100

    use again seed=42 for MCMC

--- evaluation/03_mcmc_sampling.py
+++ evaluation/03_mcmc_sampling.py
@@ -19,7 +19,7 @@ optres, = load_objects(
 )
 
 # set seed for MCMC
-tf.random.set_seed(42)
+tf.random.set_seed(42)  # again
 
 # define essential input quantities for MCMC
 optvals = optres.position
