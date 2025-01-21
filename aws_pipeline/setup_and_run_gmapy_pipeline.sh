ade54a06b40b6797a4250692200de0944dbecd12
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-01-21 17:47:21 +0100

    do not copy .git directory to s3

--- aws_pipeline/setup_and_run_gmapy_pipeline.sh
+++ aws_pipeline/setup_and_run_gmapy_pipeline.sh
@@ -21,7 +21,7 @@ done
 cd ..
 aws s3 cp "evaluation" "s3://gmapy-results/$branch_name/evaluation/" --recursive
 aws s3 cp "data" "s3://gmapy-results/$branch_name/data/" --recursive
-aws s3 cp "gmapy" "s3://gmapy-results/$branch_name/gmapy/" --recursive
+aws s3 cp "gmapy" "s3://gmapy-results/$branch_name/gmapy/" --recursive --exclude ".git/*"
 
 # terminate instance if copying successful
 if [ "$?" -eq 0 ]; then
