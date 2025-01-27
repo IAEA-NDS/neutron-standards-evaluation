fd979c2a784dc7b5eb3a450f10f26777e886bef2
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-01-27 07:40:27 +0100

    adjust plot script for SACS update

--- plotting/bokeh_plotting.py
+++ plotting/bokeh_plotting.py
@@ -36,7 +36,7 @@ from data_preparation import (
 )
 
 # only used for renormalization
-dfs = prepare_result_data('08923b6')
+dfs = prepare_result_data('363a4a8')
 exptable = dfs['exptable']
 
 # reference cross section
@@ -47,22 +47,14 @@ std2017 = dfs['std2017_dt']
 
 pred_list = []
 cols = []
-# pred_list.append(load_evaluation('01a02a0', '8007 removed', 'green', 'dotdash'))
-# pred_list.append(load_evaluation('f42e55d', '1013 to shape', 'blue', 'dotdash'))
-# pred_list.append(load_evaluation('1e8ce5e', 'recommend_new MCMC', 'orange', 'dashed'))
-# cols.append("PRED")
-pred_list.append(load_evaluation('1e8ce5e', 'latest evaluation', 'green', 'solid'))
+pred_list.append(load_evaluation('363a4a8', '+SACS update (MLE)', 'green', 'solid'))
 cols.append("MAXLIKE")
-# pred_list.append(load_evaluation('ea40e40', 'liso_rel_low_unc', 'brown', 'dotdash'))
-# cols.append("PRED")
-# pred_list.append(load_evaluation('003a588', 'liso_abs_low_unc', 'blue', 'dotdash'))
-# cols.append("PRED")
-# pred_list.append(load_evaluation('55c975c', 'liso_abs', 'black', 'solid'))
-# cols.append("PRED")
-# pred_list.append(load_evaluation('fc8634c', 'no TPC>7 MeV', 'cyan', 'dashed'))
-# cols.append("PRED")
-# pred_list.append(load_evaluation('04cc6d2', 'drop Sherbakov exp (1012) OPT', 'red', 'solid'))
-# cols.append("MAXLIKE")
+pred_list.append(load_evaluation('363a4a8', '+SACS update (MCMC)', 'brown', 'dotdash'))
+cols.append("PRED")
+pred_list.append(load_evaluation('793eb25', 'Templ+NIFFTE21 Update (MLE)', 'blue', 'dotdash'))
+cols.append("MAXLIKE")
+pred_list.append(load_evaluation('793eb25', 'Templ+NIFFTE21 Update (MCMC)', 'black', 'solid'))
+cols.append("PRED")
 
 
 endfb81_path = '/home/gschnabel/bigdata/nuclibs/endfb8.1/neutrons-version.VIII.1'
