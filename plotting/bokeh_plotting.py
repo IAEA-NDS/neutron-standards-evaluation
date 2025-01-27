3ee6906d6e2e20259b768e2f9ddcf5ceec10dec7
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-01-27 07:10:05 +0100

    adjust plot script for std2017 reeval with USU

--- plotting/bokeh_plotting.py
+++ plotting/bokeh_plotting.py
@@ -36,7 +36,7 @@ from data_preparation import (
 )
 
 # only used for renormalization
-dfs = prepare_result_data('08923b6')
+dfs = prepare_result_data('8d55148')
 exptable = dfs['exptable']
 
 # reference cross section
@@ -51,10 +51,10 @@ cols = []
 # pred_list.append(load_evaluation('f42e55d', '1013 to shape', 'blue', 'dotdash'))
 # pred_list.append(load_evaluation('1e8ce5e', 'recommend_new MCMC', 'orange', 'dashed'))
 # cols.append("PRED")
-pred_list.append(load_evaluation('1e8ce5e', 'latest evaluation', 'green', 'solid'))
+pred_list.append(load_evaluation('8d55148', 're-eval STD2017 + USU (MLE)', 'green', 'solid'))
 cols.append("MAXLIKE")
-# pred_list.append(load_evaluation('ea40e40', 'liso_rel_low_unc', 'brown', 'dotdash'))
-# cols.append("PRED")
+pred_list.append(load_evaluation('8d55148', 're-eval STD2017 + USU (MCMC)', 'brown', 'dotdash'))
+cols.append("PRED")
 # pred_list.append(load_evaluation('003a588', 'liso_abs_low_unc', 'blue', 'dotdash'))
 # cols.append("PRED")
 # pred_list.append(load_evaluation('55c975c', 'liso_abs', 'black', 'solid'))
