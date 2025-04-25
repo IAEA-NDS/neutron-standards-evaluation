66fbe6044ee547ec5163510dab7e45e9979c41a6
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-04-25 10:41:47 +0200

    evaluation with Axton and GMA db merged

--- evaluation/01_model_preparation.py
+++ evaluation/01_model_preparation.py
@@ -25,16 +25,32 @@ from gmapy.mappings.priortools import (
     remove_dummy_datasets
 )
 from gmapy.mappings.tf.restricted_map import RestrictedMap
-from gmapy.mappings.tf.energy_dependent_absolute_usu_map_tf import (
-    EnergyDependentAbsoluteUSUMap,
-    create_endep_abs_usu_df
-)
 from gmapy.tf_uq.custom_distributions import (
     MultivariateNormal,
     MultivariateNormalLikelihood,
     DistributionForParameterSubset,
     UnnormalizedDistributionProduct
 )
+
+import sys
+sys.path.append('../gmapy/tnc_axton_eval')
+import tnc_inference_prep as prep
+
+from gmapy.tf_uq.inference import (
+    determine_MAP_estimate,
+    generate_MCMC_chain,
+)
+from gmapy.mcmc_inference import compute_effective_sample_size
+
+# do a MAP
+# optres = determine_MAP_estimate(prep.startvals_tf, prep.func_and_grad_tf, prep.func_hessian_tf, ret_optres=True)
+# opt_params = (prep.trafo(optres.position)).numpy()
+
+# do MCMC
+# log_prob = lambda x: (-prep.chisquare(x))
+# chain, _ = generate_MCMC_chain(prep.startvals_tf, log_prob, prep.chisquare_hessian, num_leapfrog_steps=3, step_size=0.1, num_results=1000)
+# opt_params_mcmc = np.mean(prep.trafo(chain).numpy(), axis=0)
+
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
@@ -44,13 +60,20 @@ db_path = '../data/data.json'
 db = read_gma_database(db_path)
 remove_dummy_datasets(db['datablock_list'])
 
-priortable = create_prior_table(db['prior_list'])
+gma_priortable = create_prior_table(db['prior_list'])
 priorcov = create_prior_covmat(db['prior_list'])
 
 # prepare experimental quantities
 exptable = create_experiment_table(db['datablock_list'])
 expcov = create_experimental_covmat(db['datablock_list'])
 exptable['UNC'] = np.sqrt(expcov.diagonal())
+
+# TODO: remove the thermal neutron constants
+exptable.loc[exptable.ENERGY == 2.530000e-08]
+# find TNC values with cross correlations to higher energies
+ds_ids = exptable.loc[exptable.ENERGY == 2.530000e-08, 'NODE']
+tmp = exptable[exptable.NODE.isin(ds_ids)]
+
 
 # variation-01: remove specific experimental datasets after visual inspection
 exp_remove_mask = (exptable.NODE == 'exp_722') & (exptable.ENERGY > 23)  # Ponkratov U5(n,f) shape beyond 23 MeV
@@ -61,10 +84,21 @@ exp_remove_mask |= (exptable.NODE == 'exp_8029')
 # remove Maslov's patch
 exp_remove_mask |= (exptable.NODE == 'exp_1003')
 
+# remove Lounsbury
+exp_remove_mask |= (exptable.NODE == 'exp_8099')
+exp_remove_mask |= (exptable.NODE == 'exp_8098')
+exp_remove_mask |= (exptable.NODE == 'exp_8097')
+
+# remove Axton points
+for i in  range(910, 935):
+    exp_remove_mask |= (exptable.NODE == f'exp_{i}')
+
+
+
+
 exp_keep_idcs = np.where(~exp_remove_mask)[0]
 exptable = exptable.loc[exp_keep_idcs].reset_index(drop=True)
 expcov = csr_matrix(expcov.toarray()[np.ix_(exp_keep_idcs, exp_keep_idcs)])
-# variation-01 end
 
 # implement the recommendations of the excel sheet,
 # except the recommendation to convert the
@@ -87,12 +121,12 @@ replace_mt('exp_6001', 4, 3)
 
 
 # initialize the normalization errors
-priortable, priorcov = attach_shape_prior((priortable, exptable), covmat=priorcov, raise_if_exists=False)
-compmap = CompoundMapTF((priortable, exptable), reduce=True)
-initialize_shape_prior((priortable, exptable), compmap)
+gma_priortable, priorcov = attach_shape_prior((gma_priortable, exptable), covmat=priorcov, raise_if_exists=False)
+compmap = CompoundMapTF((gma_priortable, exptable), reduce=True)
+initialize_shape_prior((gma_priortable, exptable), compmap)
 
 # some convenient shortcuts
-priorvals = priortable.PRIOR.to_numpy()
+gma_priorvals = gma_priortable.PRIOR.to_numpy()
 expvals = exptable.DATA.to_numpy()
 
 # speed up the pdf log_prob calculations exploiting the block diagonal structure
@@ -116,8 +150,8 @@ is_adj = priorcov.diagonal() != 0.
 adj_idcs = np.where(is_adj)[0]
 fixed_idcs = np.where(~is_adj)[0]
 restrimap = RestrictedMap(
-    len(priorvals), compmap.propagate, compmap.jacobian,
-    fixed_params=priorvals[fixed_idcs], fixed_params_idcs=fixed_idcs
+    len(gma_priorvals), compmap.propagate, compmap.jacobian,
+    fixed_params=gma_priorvals[fixed_idcs], fixed_params_idcs=fixed_idcs
 )
 propfun = tf.function(restrimap.propagate)
 jacfun = tf.function(restrimap.jacobian)
@@ -130,83 +164,12 @@ expcov_linop = tf.linalg.LinearOperatorComposition(
     is_self_adjoint=True, is_positive_definite=True
 )
 
-# relevant USU error contributions
-# abs U5(n,f) at 1, 5, 15 MeV (clear USU around 2 MeV region)
-# abs PU9(n,f) at 1, 5 MeV (likely no USU but to be conservative)
-# shape U5(n,f) at 0, 1, 5, 15 MeV (likely USU in the low energy range (not thermal), at about 2 MeV nd at 15 MeV)
-# shape PU9(n,f) at 1, 5 MeV (likely USU at about 2 MeV)
-# MT:3-R1:10-R2:8 at 0, 1, 5, 15, 30, 100, 200
-# MT:3-R1:9-R2:8 at 0, 1, 5, 15, 30, 60
-# MT:4-R1:10-R2:8 at 1, 5 MeV (likely USU in 1-5 MeV range)
-# MT:4-R1:9-R2:8 at 0, 1, 5, 15 (likely USU at
-
-usu_dfs = []
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:1-R1:8',), (5e-3, 1e-1, 1., 5., 15., 30.), (1e-2,)*6))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:1-R1:9',), (5e-3, 1e-1, 1., 5., 15., 30.), (1e-2,)*6))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:2-R1:8',), (1e-3, 1e-2, 1e-1, 1., 5., 15., 30.), (1e-2,)*7))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:2-R1:9',), (1e-3, 1e-2, 1e-1, 1., 5., 15., 30.), (1e-2,)*7))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:3-R1:10-R2:8',), (0.1, 1., 5., 15., 30., 100., 200.), (1e-2,)*7))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:3-R1:9-R2:8',), (1e-3, 1e-2, 0.1, 1., 5., 15., 30., 60.), (1e-2,)*8))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:4-R1:10-R2:8',), (0.1, 1., 5., 15., 30.), (1e-2,)*5))
-usu_dfs.append(create_endep_abs_usu_df(exptable, ('MT:4-R1:9-R2:8',), (1e-3, 1e-2, 1e-1, 1., 5., 15., 30.), (1e-2,)*7))
-usu_df = pd.concat(usu_dfs, ignore_index=True)
-
-# variation-01: remove usu treatment for specific datasets (after visual inspection of results)
-usu_df = usu_df[~(usu_df.NODE == 'endep_abs_usu_521')]
-usu_df = usu_df[~(usu_df.NODE == 'endep_abs_usu_1003')]
-usu_df = usu_df[~(usu_df.NODE == 'endep_abs_usu_1028')]
-# remove USU of NIFFTE TPC PU9/U5 fission measurement, believed to very accurate
-usu_df = usu_df[~(usu_df.NODE == 'endep_abs_usu_6001')]
-usu_df = usu_df[~(usu_df.NODE == 'endep_abs_usu_6002')]
-usu_df = usu_df.reset_index(drop=True)
-# variation-01: end
-
-usu_map = EnergyDependentAbsoluteUSUMap((usu_df, exptable), reduce=True)
-usu_jac = tf.sparse.to_dense(usu_map.jacobian(usu_df.PRIOR.to_numpy()))
-
-
-
-def create_like_cov_fun(usu_df, expcov_linop, Smat):
-
-    def map_uncertainties(u):
-        ids = np.zeros((len(usu_df),), dtype=np.int32)
-        for index, row in red_usu_df.iterrows():
-            reac = row.REAC
-            energy = row.ENERGY
-            cur_idcs = usu_df.index[
-                (usu_df.REAC == reac) & (usu_df.ENERGY == energy)
-            ].to_numpy()
-            ids[cur_idcs] = index
-        # scatter the uncertainties to the appropriate places
-        tf_ids = tf.constant(ids, dtype=tf.int32)
-        uncs = tf.nn.embedding_lookup(u, tf_ids)
-        return uncs
-
-    def like_cov_fun(u):
-        uncs = map_uncertainties(u)
-        # covop = tf.linalg.LinearOperatorLowRankUpdate(
-        covop = tf.linalg.LinearOperatorLowRankUpdate(
-            expcov_linop, Smat, tf.square(uncs) + 1e-7,
-            is_self_adjoint=True, is_positive_definite=True,
-            is_diag_update_positive=True
-        )
-        return covop
-    red_usu_df = usu_df[['REAC', 'ENERGY']].drop_duplicates()
-    red_usu_df.sort_values(
-        ['REAC', 'ENERGY'], ascending=True, ignore_index=True, inplace=True
-    )
-    return like_cov_fun, red_usu_df
-
-
-like_cov_fun, red_usu_df = create_like_cov_fun(usu_df, expcov_linop, usu_jac)
-num_covpars = len(red_usu_df)
-
 # generate the prior distribution
 is_adj_constr = is_adj & np.isfinite(priorcov.diagonal())
 is_adj_constr_idcs = np.where(is_adj_constr)[0]
 priorcov_chol = tf.linalg.LinearOperatorDiag(np.sqrt(priorcov.diagonal()[is_adj_constr]))
-prior_red = MultivariateNormal(priorvals[is_adj_constr], priorcov_chol)
-prior_red.log_prob_hessian(priorvals[is_adj_constr])
+prior_red = MultivariateNormal(gma_priorvals[is_adj_constr], priorcov_chol)
+prior_red.log_prob_hessian(gma_priorvals[is_adj_constr])
 prior = DistributionForParameterSubset(
     prior_red, len(adj_idcs), is_adj_constr_idcs
 )
@@ -216,8 +179,48 @@ likelihood = MultivariateNormalLikelihood(
 )
 
 # combine prior and likelihood into posterior
-post = UnnormalizedDistributionProduct([prior, likelihood])
+
+# Replace the reaction notation in GMA reactions by the
+# notation of the Axtion reactions. Remove the GMA reactions
+# which are associated with unadjustable parameters.
+# Also construct a flag that indicates whether a GMA reactions
+# is also given in the Axton reactions.
+gma_energies = gma_priortable.ENERGY.tolist()
+orig_gma_reacs = gma_priortable.REAC.tolist()
+gma_reacs = [prep.gma_to_axt_map.get(r,r) for r in orig_gma_reacs]
+axt_reacs = prep.axt_prior_reacs
+ext_gma_reacs = [f'{r};E={e}' for r, e, a in zip(gma_reacs, gma_energies, is_adj) if a]
+ext_axt_reacs = [f'{r};E={2.53e-8}' for r in axt_reacs]
+is_axt = np.array([r in ext_axt_reacs for r in ext_gma_reacs])
+
+# Generate separate posterior distributions for the Axton database and the GMA database
+gma_post = UnnormalizedDistributionProduct([prior, likelihood])
+axt_post = prep.AxtonChiSquareDist()
+
+# Check if GMA posterior distribution is okay
+gma_priorvals = gma_priortable.loc[is_adj, 'PRIOR'].to_numpy()
+# gma_post.log_prob(gma_priorvals)
+# assert len(ext_gma_reacs) == len(gma_priorvals)
+
+# Check if Axton posterior distribution is okay
+axt_priorvals = prep.startvals
+axt_post.log_prob(axt_priorvals)
+assert len(axt_priorvals) == len(ext_axt_reacs)
+
+# For calculating the deduplicated input prior, merge GMA and Axton
+# reactions, but skip GMA reactoins that also occur in Axton
+red_ext_gma_reacs = [r for r, s in zip(ext_gma_reacs, is_axt) if not s]
+ext_reacs = red_ext_gma_reacs + ext_axt_reacs
+
+priorvals = np.sqrt(np.concatenate([gma_priorvals[~is_axt], axt_priorvals]))
+
+# Construct the posterior compound object
+post = prep.GmaAxtDist(ext_gma_reacs, gma_post, ext_axt_reacs, axt_post, ext_reacs)
+
+priortable = pd.concat([gma_priortable[is_adj][~is_axt], prep.axt_priortable], ignore_index=True)
+
+# post.log_prob(priortable.PRIOR.to_numpy())
+# post.log_prob_hessian(priorvals)
 
 save_objects('output/01_model_preparation_output.pkl', locals(),
-             'post', 'likelihood', 'priorvals', 'is_adj', 'usu_df', 'red_usu_df',
-             'num_covpars', 'priortable', 'exptable', 'expcov', 'like_cov_fun', 'compmap', 'restrimap')
+             'post', 'priorvals', 'priortable', 'exptable', 'expcov', 'compmap', 'restrimap')
--- evaluation/02_parameter_optimization.py
+++ evaluation/02_parameter_optimization.py
@@ -15,8 +15,8 @@ post, likelihood, priorvals, is_adj, usu_df, red_usu_df, num_covpars = \
 # speed it up!
 neg_log_prob_and_gradient = tf.function(post.neg_log_prob_and_gradient)
 neg_log_post_hessian = post.neg_log_prob_hessian
+refvals = tf.constant(priorvals, dtype=tf.float64)
 
-refvals = priorvals[is_adj]
 
 optres = determine_MAP_estimate(
     refvals, neg_log_prob_and_gradient,
@@ -27,8 +27,45 @@ optres = determine_MAP_estimate(
 # save the optimized parameters
 refvals = optres.position
 
+
 opt_neg_hessian = neg_log_post_hessian(optres.position)
 _, opt_neg_jac = neg_log_prob_and_gradient(optres.position)
 
+post_covmat = np.linalg.inv(opt_neg_hessian + np.diag([1e-5]*len(priortable)))
+priortable['POSTUNC'] = post_covmat.diagonal() * 4*priortable['POST'] / priortable['POST'] * 100
+
 save_objects('output/02_parameter_optimization_output.pkl', locals(),
-             'optres', 'usu_df', 'red_usu_df', 'opt_neg_hessian', 'opt_neg_jac')
+             'optres', 'opt_neg_hessian', 'opt_neg_jac')
+
+
+if True:
+    sys.exit(0)
+
+
+# look at trends
+postvals = tf.constant(priortable['POST'])
+gma_input, axt_input = post._distribute_params(postvals)
+
+predvals = propfun(gma_input)
+exptable['PRED'] = predvals
+
+exptable[['DATA', 'PRED']]
+
+
+tnc_datasets = set(exptable.loc[exptable.ENERGY == 2.53e-8, 'NODE'])
+high_datasets = set(exptable.loc[exptable.ENERGY > 2.53e-8, 'NODE'])
+both_datasets = tnc_datasets.intersection(high_datasets)
+
+import matplotlib.pyplot as plt
+
+for ds in ('exp_602', 'exp_631'): # both_datasets:
+    print(ds)
+    curds = exptable[exptable.NODE == ds]
+    plt.plot(curds['ENERGY'], (curds['DATA'] - curds['PRED']) / curds['PRED'], color='red')
+    plt.plot(curds['ENERGY'], curds['UNC'] / curds['PRED'], color='black')
+    plt.plot(curds['ENERGY'], -curds['UNC'] / curds['PRED'], color='black')
+    plt.axhline(y=0, color='gray')
+    plt.xscale('log')
+    reac = curds['REAC'].iloc[0]
+    plt.title(f'dataset: {ds} - reac: {reac}')
+    plt.show()
