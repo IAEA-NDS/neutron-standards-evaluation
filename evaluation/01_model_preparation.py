0ff4897b23828798454c920697746870e8ac99e4
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-07-28 19:44:29 +0200

    enlarge too small normalization uncertainties

--- evaluation/01_model_preparation.py
+++ evaluation/01_model_preparation.py
@@ -49,7 +49,7 @@ priorcov = create_prior_covmat(db['prior_list'])
 
 # prepare experimental quantities
 exptable = create_experiment_table(db['datablock_list'])
-expcov = create_experimental_covmat(db['datablock_list'])
+expcov = create_experimental_covmat(db['datablock_list'], relative=True)
 exptable['UNC'] = np.sqrt(expcov.diagonal())
 
 # variation-01: remove specific experimental datasets after visual inspection
@@ -63,7 +63,7 @@ exp_remove_mask |= (exptable.NODE == 'exp_1003')
 
 exp_keep_idcs = np.where(~exp_remove_mask)[0]
 exptable = exptable.loc[exp_keep_idcs].reset_index(drop=True)
-expcov = csr_matrix(expcov.toarray()[np.ix_(exp_keep_idcs, exp_keep_idcs)])
+expcov = expcov.toarray()[np.ix_(exp_keep_idcs, exp_keep_idcs)]
 # variation-01 end
 
 # implement the recommendations of the excel sheet,
@@ -85,7 +85,6 @@ replace_mt('exp_631', 3, 4)
 replace_mt('exp_1012', 3, 4)
 replace_mt('exp_6001', 4, 3)
 
-
 # initialize the normalization errors
 priortable, priorcov = attach_shape_prior((priortable, exptable), covmat=priorcov, raise_if_exists=False)
 compmap = CompoundMapTF((priortable, exptable), reduce=True)
@@ -95,13 +94,32 @@ initialize_shape_prior((priortable, exptable), compmap)
 priorvals = priortable.PRIOR.to_numpy()
 expvals = exptable.DATA.to_numpy()
 
+# modify covariance matrix (enlarge too small normalization uncertainties).
+# the following datasets have effective normalization uncertainties smaller than 1%
+expids = (
+    8030, 703, 708, 704, 705, 706, 188, 195, 125, 910,
+    911, 913, 915, 916, 917, 919, 921, 922, 923, 925, 927,
+    928, 929, 933, 934, 1039, 1040, 2001, 2016, 2013,
+    2504, 2508
+)
+expidstrs = ['exp_' + str(expid) for expid in expids]
+
+for expidstr in expidstrs:
+    curexptable = exptable[exptable.NODE == expidstr]
+    curidcs = curexptable.index
+    expcov[np.ix_(curidcs, curidcs)] += 0.01**2
+
 # speed up the pdf log_prob calculations exploiting the block diagonal structure
-expcov_list, idcs_tuples = create_datablock_covmat_list(db['datablock_list'], relative=True)
+block_sizes = exptable.DB_IDX.value_counts().sort_index().tolist()
+block_stops = np.cumsum(block_sizes)
+block_starts = np.concatenate([[0], block_stops[:-1]])
+
 # variation-01: remove certain points in datablocks
-for i in range(len(expcov_list)):
-    cur_idcs = np.arange(idcs_tuples[i][0], idcs_tuples[i][1]+1)
-    cur_idcs = cur_idcs[np.isin(cur_idcs, exp_keep_idcs)] - idcs_tuples[i][0]
-    expcov_list[i] = csr_matrix(expcov_list[i].toarray()[np.ix_(cur_idcs, cur_idcs)])
+expcov_list = []
+for i in range(len(block_sizes)):
+    cur_idcs = np.arange(block_starts[i], block_stops[i])
+    cur_expcov = expcov[np.ix_(cur_idcs, cur_idcs)]
+    expcov_list.append(csr_matrix(cur_expcov))
 
 expcov_list = [x for x in expcov_list if x.shape != (0, 0)]
 
