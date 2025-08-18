fd5e0adb170ea66da31315514ba7523864d21d1b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-08-18 15:11:44 +0200

    set usu to 0.007

--- evaluation/01_model_preparation.py
+++ evaluation/01_model_preparation.py
@@ -181,6 +181,8 @@ def create_like_cov_fun(usu_df, expcov_linop, Smat):
         return uncs
 
     def like_cov_fun(u):
+        # ad-hoc hack to see if we observe the expected uncertainty inflation
+        u = tf.constant([0.007, 0.007, 0.007], dtype=tf.float64)
         uncs = map_uncertainties(u)
         # covop = tf.linalg.LinearOperatorLowRankUpdate(
         covop = tf.linalg.LinearOperatorLowRankUpdate(
