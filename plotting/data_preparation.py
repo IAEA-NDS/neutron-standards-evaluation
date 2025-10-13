38896f9b7d39de4d734a1bc3270d803b66023232
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-10-13 10:29:46 +0200

    further extend norm checking code

--- plotting/data_preparation.py
+++ plotting/data_preparation.py
@@ -75,54 +75,29 @@ def load_evaluation(git_hash, label, color, style):
     }
 
 
-def check_normalization(propmap, priortable, exptable):
+def check_normalization(propmap, priortable, exptable, expcov):
     priortable = priortable.copy()
     exptable = exptable.copy()
 
-    mod_priortable = priortable.copy()
-    mod_priortable.loc[mod_priortable.NODE.str.match('norm_'), 'OPT'] = 1.
-
     propvals = propmap.propagate(priortable['OPT']).numpy()
-    propvals_notnorm = propmap.propagate(mod_priortable['OPT']).numpy()
     exptable['PROP'] = propvals
-    exptable['PROP_NOTNORM'] = propvals_notnorm
-
-    opt_norm_series = priortable.loc[priortable.NODE.str.match('norm_'), ['NODE', 'OPT']].copy()
-    opt_norm_series['NODE'] = opt_norm_series['NODE'].str.replace('norm_', 'exp_')
-    opt_norm_series = opt_norm_series.set_index('NODE')['OPT']
 
     red_exptable = exptable[exptable.REAC.str.match('MT:(2|4|8)-')].copy()
-    real_norm_series = red_exptable.groupby('NODE').apply(lambda group: np.mean(group['DATA'] / group['PROP_NOTNORM']))
+    real_norm_series = red_exptable.groupby('NODE').apply(lambda group: np.mean(group['DATA'] / group['PROP']))
 
-    exptable['NORM_OPT'] = 1.
-    exptable['NORM_REAL'] = 1.
-    exptable['NORM_OPT'] = exptable['NODE'].map(opt_norm_series)
-    exptable['NORM_REAL'] = exptable['NODE'].map(real_norm_series)
-
-    exptable['RSCL_PROP_OPT'] = exptable['PROP_NOTNORM'] * exptable['NORM_OPT']
-    exptable['RSCL_PROP_REAL'] = exptable['PROP'] * exptable['NORM_REAL']
-    exptable['DIFF_OPT'] = exptable['DATA'] - exptable['RSCL_PROP_OPT']
-    exptable['DIFF_REAL'] = exptable['DATA'] - exptable['RSCL_PROP_REAL']
-
-    mean_diff_opt = exptable.groupby('NODE').apply(lambda group: np.mean(group['DIFF_OPT'] / group['UNC']))
-    mean_diff_real = exptable.groupby('NODE').apply(lambda group: np.mean(group['DIFF_REAL'] / group['UNC']))
-
-    real_norm_series.index = real_norm_series.index.str.replace('exp_', 'norm_')
-    priortable['REAL_NORM'] = np.nan
-    priortable['REAL_NORM'] = priortable['NODE'].map(real_norm_series)
-
+    pd.set_option('display.max_rows', None)
+    print(real_norm_series)
+    pass
     breakpoint()
-
-    print(priortable.loc[priortable.NODE.str.match('norm_'), ['NODE', 'OPT', 'REAL_NORM']])
 
 
 @mem.cache
 def prepare_result_data(git_hash, usu_info=False, extra_info=False):
     curcalc = f'../output/{git_hash}/evaluation/output'
-    priortable, is_adj, exptable, restrimap = \
+    priortable, is_adj, exptable, restrimap, expcov = \
         load_objects(f'{curcalc}/01_model_preparation_output.pkl',
                      'priortable', 'is_adj',
-                     'exptable', 'restrimap')
+                     'exptable', 'restrimap', 'expcov')
 
     if usu_info:
         usu_df, = load_objects(
@@ -159,7 +134,7 @@ def prepare_result_data(git_hash, usu_info=False, extra_info=False):
     priortable.loc[is_adj, 'OPT'] = np.array(red_priortable['OPT'])
 
     # check normalization factors
-    check_normalization(restrimap, red_priortable, exptable)
+    check_normalization(restrimap, red_priortable, exptable, expcov)
 
     # add column where uncertainties are inflated by USU components
     # as they are used/determined in the evaluation
