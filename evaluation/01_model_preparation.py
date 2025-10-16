094c227ef5513203b7abffee6a5a3d71e03906e2
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-10-16 20:38:49 +0200

    use chisquare pseudo-dist

--- evaluation/01_model_preparation.py
+++ evaluation/01_model_preparation.py
@@ -31,7 +31,7 @@ from gmapy.mappings.tf.energy_dependent_absolute_usu_map_tf import (
 from gmapy.tf_uq.custom_distributions import (
     MultivariateNormal,
     # MultivariateNormalLikelihoodWithCovParams,
-    MultivariateNormalLikelihood,
+    ChiSquarePseudoDist,
     DistributionForParameterSubset,
     UnnormalizedDistributionProduct
 )
@@ -98,7 +98,7 @@ prior = DistributionForParameterSubset(
     prior_red, len(adj_idcs), is_adj_constr_idcs
 )
 
-likelihood = MultivariateNormalLikelihood(
+likelihood = ChiSquarePseudoDist(
     len(adj_idcs), propfun, jacfun, expvals, expcov_chol, approximate_hessian=True, relative=True
 )
 
