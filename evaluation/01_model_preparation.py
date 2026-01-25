f4e7aa6097685df0b397f5a8404244e55312a4eb
gschnabel <40870991+gschnabel@users.noreply.github.com>
2026-01-25 19:13:53 +0100

    ad-hoc shape abs->shape conversions (also from f96d2f3)

--- evaluation/01_model_preparation.py
+++ evaluation/01_model_preparation.py
@@ -63,6 +63,25 @@ exp_keep_idcs = np.where(~exp_remove_mask)[0]
 exptable = exptable.loc[exp_keep_idcs].reset_index(drop=True)
 expcov = csr_matrix(expcov.toarray()[np.ix_(exp_keep_idcs, exp_keep_idcs)])
 
+# implement the recommendations of the excel sheet,
+# except the recommendation to convert the
+# Cance 1978 Pu9, U8, U5 absolute cross sections to ratios
+def replace_mt(node, old_mt, new_mt):
+    """Change the data type (given by MT) of a dataset."""
+    t = exptable.loc[exptable.NODE == node, 'REAC']
+    t = t.str.replace(rf'^MT:{old_mt}', f'MT:{new_mt}', regex=True)
+    exptable.loc[exptable.NODE == node, 'REAC'] = t
+
+replace_mt('exp_602', 3, 4)
+replace_mt('exp_685', 3, 4)
+replace_mt('exp_605', 3, 4)
+replace_mt('exp_666', 3, 4)
+replace_mt('exp_600', 3, 4)
+replace_mt('exp_608', 3, 4)
+replace_mt('exp_631', 3, 4)
+replace_mt('exp_1012', 3, 4)
+replace_mt('exp_6001', 4, 3)
+
 # speed up the pdf log_prob calculations exploiting the block diagonal structure
 block_lens = exptable['DB_IDX'].value_counts().sort_index().to_numpy()
 block_stops = np.cumsum(block_lens)
