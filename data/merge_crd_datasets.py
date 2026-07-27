4d6a137b82fc8483032d5560b611aaf7b5598ebc
gschnabel <40870991+gschnabel@users.noreply.github.com>
2026-07-27 17:08:30 +0200

    add finalized CRD/GMA merge script

--- data/merge_crd_datasets.py
+++ data/merge_crd_datasets.py
@@ -15,26 +15,56 @@ db['datablocks'] = db.pop('datablock_list')  # for consistent naming
 db['prior'] = db.pop('prior_list')  # for consistent naming
 
 # load reduced CRD file
-red_crd_path = os.path.join('reduced_crd', 'file')
-with open('red_crd_path', 'r') as f:
+red_crd_path = os.path.join('reduced_crd_files', 'LA-UR-25-32229-1_textfile.json')
+with open(red_crd_path, 'r') as f:
     crd_red = json.load(f)
 
+# get datablock/dataset mapping for datasets in GMA database
+gma_dataset_map = {}
+for blck_idx, blck in enumerate(db['datablocks']):
+    for ds_idx, ds in enumerate(blck['datasets']):
+        if 'NS' in ds:
+            ds_id = ds['NS']
+        else:
+            ds_id = ds['identifier']
+        curlist = gma_dataset_map.setdefault(ds_id, [])
+        curlist.append((blck_idx, ds_idx))
+
+# get datablock/dataset mapping for datasets in GMA database
+crd_dataset_map = {}
+for blck_idx, blck in enumerate(crd_red['datablocks']):
+    for ds_idx, ds in enumerate(blck['datasets']):
+        if 'NS' in ds:
+            ds_id = ds['NS']
+        else:
+            ds_id = ds['identifier']
+        curlist = crd_dataset_map.setdefault(ds_id, [])
+        curlist.append((blck_idx, ds_idx))
+
 # merge CRD file into GMA
-db['datablocks'].insert(0, crd_red['datablocks'][0])
+new_blocks = {}
+for key, lst in crd_dataset_map.items():
+    crd_blck_idx, crd_ds_idx = lst[0]
+    new_dataset = crd_red['datablocks'][crd_blck_idx]['datasets'][crd_ds_idx]
+    # if dataset id already exists in GMA, replace dataset
+    if key in gma_dataset_map:
+        gma_lst = gma_dataset_map[key]
+        if len(gma_lst) > 1:
+            raise IndexError(f'Ambiguous dataset id for {key}')
+        gma_blck_idx, gma_ds_idx = gma_lst[0]
+        db['datablocks'][gma_blck_idx]['datasets'][gma_ds_idx] = new_dataset
+        print(f'replaced {key} in GMA database by dataset in CRD file')
+    # otherwise, add the dataset
+    else:
+        new_lst = new_blocks.setdefault(key, [])
+        new_lst.append(new_dataset)
+        print(f'added new dataset {key} to GMA database')
+
+db['datablocks'] += list(new_blocks.values())
+
 
 # write to disk
 with open(modified_dbpath, 'w') as f:
     json.dump(db, f, cls=JSONEncoder,values_per_line=10, min_list_len=15, indent=2)
 
 
-# check duplicates
-dataset_ids = [ ds['NS']  for dblock in db['datablocks'] for ds in dblock['datasets']]
-seen = set()
-duplicates = set()
-for item in dataset_ids:
-    if item in seen:
-        duplicates.add(item)
-    else:
-        seen.add(item)
-
-print(f'duplicates: {duplicates}')
